e173d3c6dd2271cf1936d044e27962ec87a63e31
Nimna Gunasekera <97610809+nimnagunasekera@users.noreply.github.com>
2024-10-24 17:28:03 +0530

    Update policy.blade.php

--- resources/views/policy.blade.php
+++ resources/views/policy.blade.php
@@ -1,11 +1,11 @@
 <x-guest-layout>
     <div class="pt-4 bg-gray-100">
-        <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
+        <div class="min-h-screen flex flex-col items-center p-4 sm:p-4">
             <a href="/">
                 <x-authentication-card-logo />
             </a>
 
-            <div class="w-full sm:max-w-2xl mt-6 p-6 bg-white shadow-md overflow-hidden sm:rounded-lg prose">
+            <div class="w-full lg:max-w-5xl sm:max-w-2xl my-6 p-6 bg-white shadow-md overflow-hidden sm:rounded-lg prose">
                 <h1 class="text-2xl">{{ $policy->title ?? 'No Title Available' }}</h1>
                 <div>
                     {!! $policy->description ?? 'No Description Available' !!}
